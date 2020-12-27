bc78aaf27ac575b5f667de18a43e70ff8f2dfa76
velut <12040076+velut@users.noreply.github.com>
2020-12-27 18:56:28 +0100

    feat: add plausible.io script

--- src/components/common/Head.tsx
+++ src/components/common/Head.tsx
@@ -9,6 +9,17 @@ export function Head() {
                 name="viewport"
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
+
+            {/* Plausible.io analytics */}
+            {/* See https://github.com/vercel/next.js/issues/9070#issuecomment-552981178 */}
+            {process.env.NODE_ENV === 'production' && process.browser && (
+                <script
+                    async
+                    defer
+                    data-domain="videospeedup.com"
+                    src="https://plausible.io/js/plausible.js"
+                />
+            )}
         </NextHead>
     );
 }
