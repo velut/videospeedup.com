fdbc49ebad6b07f9912be7fdc59c9a301ba624ed
velut <12040076+velut@users.noreply.github.com>
2020-12-27 03:03:01 +0100

    feat: update navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,3 +1,12 @@
 export function Navbar() {
-    return <header></header>;
+    return (
+        <header className="p-5 text-gray-100 bg-gray-800">
+            <h1>VideoSpeedup.com</h1>
+
+            <p className="mt-1 text-lg font-bold">
+                Easily adjust video playback speed on Youtube, Vimeo, Netflix,
+                Prime Video and many more.
+            </p>
+        </header>
+    );
 }
