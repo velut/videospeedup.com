cefbded25c5c6162decd6ea9b40e6d9977e62dbd
velut <12040076+velut@users.noreply.github.com>
2020-12-28 03:05:02 +0100

    feat: add how to section

---
+++ src/components/sections/CreditsSection.tsx
@@ -0,0 +1,17 @@
+import { A } from '../common/A';
+
+export function CreditsSection() {
+    return (
+        <section>
+            <h2>Credits</h2>
+
+            <p>
+                The video featured in the tutorials is{' '}
+                <A href="https://www.youtube.com/watch?v=Lsq0FiXjGHg">
+                    Ticking Digital Clock
+                </A>{' '}
+                by DrLex1.
+            </p>
+        </section>
+    );
+}
