1e1474378792521f6805ea0ed3e6467544e6168e
velut <12040076+velut@users.noreply.github.com>
2020-12-28 18:14:34 +0100

    feat: link to homepage from navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,7 +1,15 @@
+import { Link } from './Link';
+
 export function Navbar() {
     return (
         <header className="p-5 text-gray-100 bg-gray-800 border-b border-gray-700 dark:bg-gray-900">
-            <h1>VideoSpeedup.com</h1>
+            <h1>
+                <Link href="/">
+                    <span className="font-bold text-gray-900 hover:underline dark:text-gray-100">
+                        VideoSpeedup.com
+                    </span>
+                </Link>
+            </h1>
 
             <p className="mt-1 text-lg font-bold">
                 Easily adjust video playback speed on Youtube, Vimeo, Netflix,
