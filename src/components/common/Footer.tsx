d8396f9b1e0cded0c231ee8cc112e54042ce76f0
velut <12040076+velut@users.noreply.github.com>
2020-12-28 01:28:48 +0100

    feat: update footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,17 +1,28 @@
+import { A } from './A';
 import { Link } from './Link';
 
 export function Footer() {
     return (
         <footer className="p-5 bg-gray-100 border-t border-gray-300 dark:bg-gray-900 dark:border-gray-700 md:flex md:items-center md:justify-between">
-            <Link href="/">
-                <span className="font-bold text-gray-900 hover:underline dark:text-gray-100">
-                    VideoSpeedup.com
-                </span>
-            </Link>
+            <p className="mt-0">
+                <Link href="/">
+                    <span className="font-bold text-gray-900 hover:underline dark:text-gray-100">
+                        VideoSpeedup.com
+                    </span>
+                </Link>{' '}
+                created by{' '}
+                <A href="https://github.com/velut">Edoardo Scibona</A>
+            </p>
 
             <ul className="mt-4 space-y-1 md:mt-0 sm:flex sm:space-x-4 sm:space-y-0">
                 <li>
                     <Link href="/privacy">Privacy Policy</Link>
+                </li>
+
+                <li>
+                    <A href="https://github.com/velut/videospeedup.com/issues">
+                        Report an Issue
+                    </A>
                 </li>
             </ul>
         </footer>
