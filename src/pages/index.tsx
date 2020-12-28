46897c5229f8d5c639065a8ed0a6145fc14a9761
velut <12040076+velut@users.noreply.github.com>
2020-12-28 03:05:43 +0100

    feat: update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,33 @@
 import NextHead from 'next/head';
 import { Layout } from '../components/common/Layout';
+import { CreditsSection } from '../components/sections/CreditsSection';
+import { CustomSpeedSection } from '../components/sections/CustomSpeedSection';
+import { HowToSection } from '../components/sections/HowToSection';
+import { SlowdownSection } from '../components/sections/SlowdownSection';
+import { SpeedupSection } from '../components/sections/SpeedupSection';
 
 export default function IndexPage() {
-    const pageTitle = 'Index page';
+    const pageTitle =
+        'VideoSpeedup.com | Easily adjust video playback speed on Youtube, Vimeo, Netflix, Prime Video and many more.';
+
+    const pageDescription =
+        'VideoSpeedup.com provides easy to use bookmarklets that let you adjust video playback speed on Youtube, Vimeo, Netflix, Prime Video and many more video platforms.';
 
     return (
         <>
             <NextHead>
                 <title>{pageTitle}</title>
+                <meta name="description" content={pageDescription} />
             </NextHead>
 
             <Layout>
-                <h1>hello</h1>
+                <div className="space-y-12">
+                    <HowToSection />
+                    <SpeedupSection />
+                    <SlowdownSection />
+                    <CustomSpeedSection />
+                    <CreditsSection />
+                </div>
             </Layout>
         </>
     );
