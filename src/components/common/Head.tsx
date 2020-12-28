3a0ad629b9b5d9fa9769504099e6108919c63f0a
velut <12040076+velut@users.noreply.github.com>
2020-12-28 19:06:35 +0100

    feat: add global open graph tags

--- src/components/common/Head.tsx
+++ src/components/common/Head.tsx
@@ -10,6 +10,26 @@ export function Head() {
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
 
+            {/* Global Open Graph tags */}
+            <meta property="og:type" content="website" />
+            <meta
+                property="og:image"
+                content="https://www.videospeedup.com/logo-512.png"
+            />
+            <meta property="og:image:alt" content="VideoSpeedup.com logo" />
+            <meta property="og:site_name" content="VideoSpeedup.com" />
+
+            {/* Global Twitter card tags */}
+            <meta property="twitter:card" content="summary" />
+            <meta
+                property="twitter:image"
+                content="https://www.videospeedup.com/logo-512.png"
+            />
+            <meta
+                property="twitter:image:alt"
+                content="VideoSpeedup.com logo"
+            />
+
             {/* Plausible.io analytics */}
             {/* See https://github.com/vercel/next.js/issues/9070#issuecomment-552981178 */}
             {process.env.NODE_ENV === 'production' && process.browser && (
