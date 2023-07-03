f6b2341479c37e5c232ff71641062012516f2000
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-03 20:22:21 +0200

    feat: add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
+	import {
+		githubCommitShort,
+		githubCommitUrl,
+		githubUrl,
+		projectNameStylized
+	} from '$lib/metadata';
+</script>
+
+<footer class="footer bg-base-300 p-8 text-base text-base-content">
+	<div>
+		<a href="/" class="link-hover link flex items-center gap-2">
+			<div class="text-lg font-bold">⏩ {projectNameStylized}</div>
+		</a>
+		<p>
+			Created by <a
+				href="https://www.edoardoscibona.com"
+				target="_blank"
+				class="link-hover link font-bold">Edoardo Scibona</a
+			>
+		</p>
+		<p class="text-sm">
+			Commit: <a href={githubCommitUrl} target="_blank" class="link-hover link">
+				{githubCommitShort}
+			</a>
+		</p>
+	</div>
+
+	<div>
+		<span class="footer-title opacity-80">Links</span>
+		<a href="/" class="link-hover link">Homepage</a>
+		<a href={githubUrl} target="_blank" class="link-hover link">GitHub Project</a>
+	</div>
+
+	<div>
+		<span class="footer-title opacity-80">Legal</span>
+		<a href="/privacy/" class="link-hover link">Privacy Policy</a>
+		<a href="/licenses/" class="link-hover link">Software Licenses</a>
+	</div>
+
+	<div>
+		<span class="footer-title opacity-80">Theme</span>
+		<ThemeSwitch />
+	</div>
+</footer>
