2cbd4ce88d707b454af69525cc3f854a09da810c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-03 20:11:33 +0200

    feat: add metadata

---
+++ src/lib/metadata.ts
@@ -0,0 +1,15 @@
+export const projectName = 'videospeedup.com';
+export const baseUrl = `https://www.videospeedup.com`;
+export const githubUrl = `https://github.com/velut/${projectName}`;
+export const githubIssuesUrl = `${githubUrl}/issues`;
+export const githubCommit = _GIT_COMMIT;
+export const githubCommitShort = githubCommit.slice(0, 7);
+export const githubCommitUrl = `${githubUrl}/commit/${githubCommit}`;
+
+export const pageTitle = (title: string) => {
+	return `${title} | ${projectName}`;
+};
+
+export const pageUrl = (path: string) => {
+	return path === '' ? baseUrl : `${baseUrl}/${path}/`;
+};
