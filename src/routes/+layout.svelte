c892eafd249e0c7910e4882f7ba6370b281e001a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-03 20:23:24 +0200

    feat: add global layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Footer from '$lib/components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import '../app.css';
+</script>
+
+<div class="flex min-h-screen flex-col">
+	<Navbar />
+
+	<div class="grow">
+		<slot />
+	</div>
+
+	<Footer />
+</div>
