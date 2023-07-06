5208b307c8f4a0bcdcf6d472862f466f735821a7
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-06 20:44:04 +0200

    build: add @tailwindcss/typography license back

--- vite-plugin-licenses.ts
+++ vite-plugin-licenses.ts
@@ -13,14 +13,14 @@ type License = {
 const registryUrl = 'https://www.npmjs.com/package';
 
 const manualLicenses: License[] = [
-	// {
-	// 	name: '@tailwindcss/typography',
-	// 	version: packageJson.devDependencies['@tailwindcss/typography'].replace('^', ''),
-	// 	license: 'MIT',
-	// 	licenseText:
-	// 		'MIT License\n\nCopyright (c) Tailwind Labs, Inc.\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.',
-	// 	url: 'https://github.com/tailwindlabs/tailwindcss-typography'
-	// },
+	{
+		name: '@tailwindcss/typography',
+		version: packageJson.devDependencies['@tailwindcss/typography'].replace('^', ''),
+		license: 'MIT',
+		licenseText:
+			'MIT License\n\nCopyright (c) Tailwind Labs, Inc.\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.',
+		url: 'https://github.com/tailwindlabs/tailwindcss-typography'
+	},
 	{
 		name: 'daisyui',
 		version: packageJson.devDependencies['daisyui'].replace('^', ''),
