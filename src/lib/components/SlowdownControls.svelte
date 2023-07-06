e42351fb467644355ad7ba2906a5672152505c31
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-07 01:14:00 +0200

    feat: add slowdown controls

---
+++ src/lib/components/SlowdownControls.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import SpeedControl from '$lib/components/SpeedControl.svelte';
+
+	const speeds = [0.1, 0.25, 0.5, 0.75];
+</script>
+
+<h2>⏪ Slowdown Controls</h2>
+
+<p>
+	Drag and drop the slowdown controls available below to your browser bookmarks bar and click on
+	them to watch videos at slower speeds.
+</p>
+
+<div class="not-prose grid grid-cols-2 gap-4 mt-6 sm:gap-6 sm:grid-cols-4">
+	{#each speeds as speed}
+		<SpeedControl {speed} />
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import HowTo from '$lib/components/HowTo.svelte';
 	import Metadata from '$lib/components/Metadata.svelte';
+	import SlowdownControls from '$lib/components/SlowdownControls.svelte';
 	import SpeedupControls from '$lib/components/SpeedupControls.svelte';
 	import { projectNameStylized } from '$lib/metadata';
 </script>
@@ -11,11 +12,14 @@
 />
 
 <main class="flex items-center justify-center p-4 sm:p-8">
-	<article class="prose">
+	<article class="prose lg:prose-lg">
 		<HowTo />
 		<div class="divider" />
 
 		<SpeedupControls />
+		<div class="divider" />
+
+		<SlowdownControls />
 		<div class="divider" />
 	</article>
 </main>
