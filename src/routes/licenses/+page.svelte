be9dc8f59279d61b83ef264753edf895919c3a0d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-06 20:34:25 +0200

    feat: add /licenses page

---
+++ src/routes/licenses/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Metadata from '$lib/components/Metadata.svelte';
+	import { licenses } from '$lib/licenses';
+	import { projectNameStylized } from '$lib/metadata';
+</script>
+
+<Metadata
+	title="Licenses for Third Party Software"
+	description="Licenses for third party software used on {projectNameStylized}"
+	path="licenses"
+/>
+
+<main class="flex items-center justify-center p-4 sm:p-8">
+	<article class="prose">
+		<hgroup>
+			<h1>Licenses for Third Party Software</h1>
+			<p>This is a list of third party software and libraries used on {projectNameStylized}.</p>
+		</hgroup>
+
+		<div class="divider" />
+
+		<ul>
+			{#each licenses as license}
+				<li>
+					<a href={license.url} target="_blank" class="link-hover link font-bold">
+						{license.name}
+					</a>
+
+					<p>
+						Version: {license.version}
+						<br />
+						License: {license.license}
+					</p>
+
+					{#if license.licenseText}
+						<pre class="w-64 sm:w-full">
+							{license.licenseText}
+						</pre>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+
+		<div class="divider" />
+	</article>
+</main>
