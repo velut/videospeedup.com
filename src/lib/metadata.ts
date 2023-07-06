c74d76b4682859d12d12209de9cc2d0aa86ca91f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-06 20:38:04 +0200

    refactor: use stylized project name in metadata

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -8,7 +8,7 @@ export const githubCommitShort = githubCommit.slice(0, 7);
 export const githubCommitUrl = `${githubUrl}/commit/${githubCommit}`;
 
 export const pageTitle = (title: string) => {
-	return `${title} | ${projectName}`;
+	return `${title} | ${projectNameStylized}`;
 };
 
 export const pageUrl = (path: string) => {
