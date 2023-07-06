a5f85b1fd8ef106483838324769c13a5430cfb31
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-07 00:37:30 +0200

    feat: add HowTo section

---
+++ src/lib/components/HowTo.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { projectNameStylized } from '$lib/metadata';
+</script>
+
+<h2>📖 How-to</h2>
+
+<p>
+	With <strong>{projectNameStylized}</strong> you can easily adjust video playback speed on Youtube,
+	Twitch, Vimeo, Netflix, Prime Video and many more video platforms.
+</p>
+
+<p>
+	To use the speed controls available below, <strong>follow these steps</strong>:
+</p>
+
+<ol>
+	<li>
+		<p>
+			Press <strong>Ctrl + Shift + B</strong> (or <strong>Cmd + Shift + B</strong> on Mac) to show
+			the bookmarks or favorites bar in your browser. See detailed instructions for
+			<a href="https://support.google.com/chrome/answer/188842?hl=en">Google Chrome</a>,
+			<a href="https://support.mozilla.org/en-US/kb/bookmarks-toolbar-display-favorite-websites"
+				>Mozilla Firefox</a
+			>,
+			<a
+				href="https://support.microsoft.com/en-us/microsoft-edge/see-your-favorites-bar-in-microsoft-edge-and-internet-explorer-11-566736f8-a11d-52e2-4f6f-7a713e1750d2"
+				>Microsoft Edge</a
+			>, and
+			<a href="https://support.apple.com/guide/safari/customize-the-safari-window-ibrw1012/mac"
+				>Apple Safari</a
+			>.
+		</p>
+	</li>
+
+	<li>
+		<p>
+			<strong>Drag and drop</strong> the desired speed controls from this page
+			<strong> to the bookmarks bar </strong> which is now visible. This will add the speed controls
+			to your browser ready to use.
+		</p>
+	</li>
+
+	<li>
+		<p>
+			<strong>When watching a video</strong>, click on the speed controls on the bookmarks bar to
+			change the playback speed. Click the speed controls again to reset the playback speed to
+			normal.
+		</p>
+	</li>
+</ol>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import HowTo from '$lib/components/HowTo.svelte';
+	import Metadata from '$lib/components/Metadata.svelte';
+	import { projectNameStylized } from '$lib/metadata';
+</script>
+
+<Metadata
+	title="Watch videos faster"
+	description="With {projectNameStylized} you can watch videos faster."
+/>
+
+<main class="flex items-center justify-center p-4 sm:p-8">
+	<article class="prose">
+		<HowTo />
+		<div class="divider" />
+	</article>
+</main>
