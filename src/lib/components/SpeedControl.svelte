564cbd2f6484acc2356bb9a58eec2e5ad31c91a7
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-08 19:28:11 +0200

    feat: update icons for speed control

--- src/lib/components/SpeedControl.svelte
+++ src/lib/components/SpeedControl.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import FastForwardIcon from '$lib/components/FastForwardIcon.svelte';
+	import FastReverseIcon from '$lib/components/FastReverseIcon.svelte';
+
 	export let speed: number;
 
 	const js = `javascript:(function(){var v=document.querySelector('video');v.playbackRate=${speed};})();`;
-
-	$: icon = speed >= 1 ? '⏩' : '⏪';
 </script>
 
 <a href={js} class="btn btn-lg normal-case btn-neutral shadow-lg">
-	{icon}
+	{#if speed >= 1}
+		<FastForwardIcon class="h-5 w-5" />
+	{:else}
+		<FastReverseIcon class="h-5 w-5" />
+	{/if}
 	{speed}x
 </a>
