1e28933686f1f1ad86d9ad756db1c9e80e016055
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-10 00:26:51 +0200

    refactor: move intro to own component

---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { projectNameStylized } from '$lib/metadata';
+</script>
+
+<p>
+	With <strong>{projectNameStylized}</strong> you can easily adjust the playback speed of videos on Youtube,
+	Twitch, Vimeo, Netflix, Prime Video and many more video platforms.
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import CustomSpeedControl from '$lib/components/CustomSpeedControl.svelte';
 	import HowTo from '$lib/components/HowTo.svelte';
+	import Intro from '$lib/components/Intro.svelte';
 	import Metadata from '$lib/components/Metadata.svelte';
 	import SlowdownControls from '$lib/components/SlowdownControls.svelte';
 	import SpeedupControls from '$lib/components/SpeedupControls.svelte';
@@ -15,10 +16,7 @@
 <main class="flex items-center justify-center p-4 sm:p-8">
 	<article class="prose">
 		<div class="divider" />
-		<p>
-			With <strong>{projectNameStylized}</strong> you can easily adjust the playback speed of videos
-			on Youtube, Twitch, Vimeo, Netflix, Prime Video and many more video platforms.
-		</p>
+		<Intro />
 		<div class="divider" />
 
 		<HowTo />
