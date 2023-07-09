5ed6e0f0bb47432e4952ba0f4a321b8b99fcc729
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-07-10 01:35:42 +0200

    feat: change bookmarklet based on optionFixedSpeed

--- src/lib/components/SpeedControl.svelte
+++ src/lib/components/SpeedControl.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { optionFixedSpeed } from '$lib/stores';
 	import fastForwardButton from '@iconify/icons-noto/fast-forward-button';
 	import fastReverseButton from '@iconify/icons-noto/fast-reverse-button';
 	import Icon from '@iconify/svelte';
@@ -6,7 +7,9 @@
 	export let speed: number;
 
 	$: icon = speed >= 1 ? fastForwardButton : fastReverseButton;
-	$: js = `javascript:(function(){var v=document.querySelector('video');v.playbackRate=${speed};})();`;
+	$: js = $optionFixedSpeed
+		? `javascript:(function(){var v=document.querySelector('video');v.playbackRate=${speed};})();`
+		: `javascript:(function(){var v=document.querySelector('video');v.playbackRate=v.playbackRate!==1?1:${speed};})();`;
 </script>
 
 <a href={js} class="btn btn-lg normal-case btn-outline shadow-lg text-lg">
