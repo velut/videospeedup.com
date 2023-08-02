03a7305deb14c3f1dc4738d561c36c10779910c2
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-08-02 19:30:38 +0200

    feat: add explanations for available options

--- src/lib/components/Options.svelte
+++ src/lib/components/Options.svelte
@@ -13,11 +13,17 @@
 	behavior, you will have to drag and drop the speed controls again.
 </p>
 
+<div class="divider" />
+
 <div class="form-control">
 	<label class="label cursor-pointer gap-6">
 		<span class="label-text text-base"
-			>Speed controls only set their speed and never reset back to 1x.</span
-		>
+			><span class="font-bold">Speed controls just set their speed and never reset back to 1x.</span
+			><br /><span class="text-sm"
+				>By default, speed controls alternate between their speed (for example, 3x) and the default
+				1x speed. Turn on this feature to have them just set their speed without going back to 1x.</span
+			>
+		</span>
 		<input
 			id="option-fixed-speed"
 			type="checkbox"
@@ -27,19 +33,33 @@
 	</label>
 </div>
 
+<div class="divider" />
+
 <div class="form-control">
 	<label class="label cursor-pointer gap-6">
 		<span class="label-text text-base"
-			>Speed controls affect all media on a page and not just one element.</span
+			><span class="font-bold">Speed controls affect all media elements on a page.</span><br /><span
+				class="text-sm"
+				>By default, speed controls only affect the first video in a page. Turn on this feature to
+				have them affect all videos in a page. This lets you watch multiple videos on Youtube Shorts
+				or Twitter at faster speeds.</span
+			></span
 		>
 		<input id="option-all-media" type="checkbox" class="toggle" bind:checked={$optionAllMedia} />
 	</label>
 </div>
 
+<div class="divider" />
+
 <div class="form-control">
 	<label class="label cursor-pointer gap-6">
 		<span class="label-text text-base"
-			>Speed controls also work on audio elements and not just video ones.</span
+			><span class="font-bold">Speed controls also affect audio-only elements.</span><br /><span
+				class="text-sm"
+				>By default, speed controls only affect video elements in a page. Turn on this feature to
+				have them also affect audio-only elements in a page ("All media elements" feature may also
+				be needed for this to work).</span
+			></span
 		>
 		<input
 			id="option-include-audio"
