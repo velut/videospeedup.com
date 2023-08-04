bb69b1816769975fd1a77de4ae3e59c109b2672c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-08-04 19:31:12 +0200

    feat: update options

--- src/lib/components/Options.svelte
+++ src/lib/components/Options.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import HammerAndWrenchIcon from '$lib/components/HammerAndWrenchIcon.svelte';
+	import InfinityIcon from '$lib/components/InfinityIcon.svelte';
+	import MusicalNoteIcon from '$lib/components/MusicalNoteIcon.svelte';
+	import RepeatSingleIcon from '$lib/components/RepeatSingleIcon.svelte';
 	import { optionAllMedia, optionFixedSpeed, optionIncludeAudio } from '$lib/stores';
 </script>
 
@@ -18,8 +21,10 @@
 <div class="form-control">
 	<label class="label cursor-pointer gap-6">
 		<span class="label-text text-base"
-			><span class="font-bold">Speed controls just set their speed and never reset back to 1x.</span
-			><br /><span class="text-sm"
+			><span class="flex items-center gap-2 font-bold"
+				><RepeatSingleIcon class="h-5 w-5 flex-none" /> Speed controls just set their speed and never
+				1x.</span
+			><span class="mt-2 block text-sm"
 				>By default, speed controls alternate between their speed (for example, 3x) and the default
 				1x speed. Turn on this feature to have them just set their speed without going back to 1x.</span
 			>
@@ -38,11 +43,12 @@
 <div class="form-control">
 	<label class="label cursor-pointer gap-6">
 		<span class="label-text text-base"
-			><span class="font-bold">Speed controls affect all media elements on a page.</span><br /><span
-				class="text-sm"
+			><span class="flex items-center gap-2 font-bold"
+				><InfinityIcon class="h-5 w-5 flex-none" /> Speed controls affect all media elements on a page.</span
+			><span class="mt-2 block text-sm"
 				>By default, speed controls only affect the first video in a page. Turn on this feature to
-				have them affect all videos in a page. This lets you watch multiple videos on Youtube Shorts
-				or Twitter at faster speeds.</span
+				have them affect all videos in a page. This lets you watch multiple videos on TikTok,
+				Youtube Shorts or Twitter at faster speeds.</span
 			></span
 		>
 		<input id="option-all-media" type="checkbox" class="toggle" bind:checked={$optionAllMedia} />
@@ -54,8 +60,9 @@
 <div class="form-control">
 	<label class="label cursor-pointer gap-6">
 		<span class="label-text text-base"
-			><span class="font-bold">Speed controls also affect audio-only elements.</span><br /><span
-				class="text-sm"
+			><span class="flex items-center gap-2 font-bold"
+				><MusicalNoteIcon class="h-5 w-5 flex-none" /> Speed controls also affect audio-only elements.</span
+			><span class="mt-2 block text-sm"
 				>By default, speed controls only affect video elements in a page. Turn on this feature to
 				have them also affect audio-only elements in a page (The "All media elements" feature above
 				may also be needed for this to work).</span
