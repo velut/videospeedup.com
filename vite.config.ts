b50b6185ff4edbcc98f75d3bad42f06b6b584300
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-01 02:29:20 +0100

    feat: upgrade tailwind to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { execSync } from 'node:child_process';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
@@ -6,6 +7,7 @@ import { licenses } from './vite-plugin-licenses';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte'
