d83f951920144f1c9be347112982ab887b56f9a1
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-03 19:42:29 +0100

    refactor: update navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,7 @@
 <script>
-	import { projectNameStylized } from '$lib/metadata';
 	import IconFastForward from '~icons/noto/fast-forward-button';
 </script>
 
 <div class="navbar bg-base-100 shadow-sm">
-	<a href="/" class="btn btn-ghost text-xl"
-		><IconFastForward class="size-6" /> {projectNameStylized}</a
-	>
+	<a href="/" class="btn btn-ghost text-xl"><IconFastForward class="size-6" /> VideoSpeedup.com</a>
 </div>
