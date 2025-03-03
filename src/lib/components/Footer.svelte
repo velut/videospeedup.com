5b708b9872d5aec47b618e01fd1050429cc59a46
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-03 19:59:04 +0100

    feat: update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,33 +1,30 @@
 <script lang="ts">
 	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
-	import {
-		githubCommitShort,
-		githubCommitUrl,
-		githubUrl,
-		projectNameStylized
-	} from '$lib/metadata';
 	import IconFastForward from '~icons/noto/fast-forward-button';
+
+	const authorUrl = `https://github.com/velut`;
+	const ghUrl = `https://github.com/velut/videospeedup.com`;
+	const ghCommitUrl = `${ghUrl}/commit/${__GIT_COMMIT}`;
+	const ghCommitShort = __GIT_COMMIT.slice(0, 7);
 </script>
 
 <footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
 	<div>
 		<a href="/" class="link-hover link flex items-center gap-2 text-xl font-bold"
-			><IconFastForward class="h-6 w-6" /> {projectNameStylized}</a
+			><IconFastForward class="size-6" /> VideoSpeedup.com</a
 		>
 		<p>
-			Created by <a href="https://www.edoardoscibona.com" class="link-hover link font-bold"
-				>Edoardo Scibona</a
-			>
+			Created by <a href={authorUrl} class="link-hover link font-bold">Edoardo Scibona</a>
 		</p>
 		<p class="text-sm">
-			Commit: <a href={githubCommitUrl} class="link-hover link">{githubCommitShort}</a>
+			Commit: <a href={ghCommitUrl} class="link-hover link">{ghCommitShort}</a>
 		</p>
 	</div>
 
 	<div>
 		<span class="footer-title opacity-80">Links</span>
 		<a href="/" class="link-hover link">Homepage</a>
-		<a href={githubUrl} class="link-hover link">GitHub project</a>
+		<a href={ghUrl} class="link-hover link">GitHub project</a>
 	</div>
 
 	<div>
