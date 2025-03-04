fd462c84425f096dc25d92e1121726da2eb7ca1b
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-04 03:15:48 +0100

    feat: update licenses page

--- src/routes/licenses/+page.svelte
+++ src/routes/licenses/+page.svelte
@@ -1,27 +1,19 @@
 <script lang="ts">
-	// import Metadata from '$lib/components/Metadata.svelte';
 	import { licenses } from '$lib/licenses';
-	import { projectNameStylized } from '$lib/metadata';
 </script>
-
-<!-- <Metadata
-	title="Licenses for Third Party Software"
-	description="Licenses for third party software used on {projectNameStylized}"
-	path="licenses"
-/> -->
 
 <main class="flex items-center justify-center p-4 sm:p-8">
 	<article class="prose">
-		<div class="divider"></div>
-		<h1 class="my-0 text-center">Software Licenses</h1>
-		<div class="divider"></div>
+		<hr />
+		<h1 class="my-0 text-center">Software licenses</h1>
+		<hr />
 
-		<p>This is a list of third party software and libraries used on {projectNameStylized}.</p>
+		<p>This is a list of third-party software and libraries used on VideoSpeedup.com.</p>
 
 		<ul>
 			{#each licenses as license}
 				<li>
-					<a href={license.url} target="_blank" class="link-hover link font-bold">
+					<a href={license.url} class="link-hover link font-bold">
 						{license.name}
 					</a>
 
@@ -40,6 +32,6 @@
 			{/each}
 		</ul>
 
-		<div class="divider"></div>
+		<hr />
 	</article>
 </main>
---
+++ src/routes/licenses/+page.ts
@@ -0,0 +1,6 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = () => ({
+	title: 'Licenses for third-party software',
+	description: 'Licenses for third party software used on VideoSpeedup.com'
+});
