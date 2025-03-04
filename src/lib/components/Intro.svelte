5e613757be189d3b67250596d152a684d5b49347
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-04 02:21:10 +0100

    feat: update Intro

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -1,6 +1,8 @@
-<div class="flex justify-center">
-	<p class="prose my-0 lg:max-w-[75ch]">
+<div class="flex flex-col items-center">
+	<div class="divider"></div>
+	<p class="text-center text-balance lg:max-w-[75ch]">
 		<strong>VideoSpeedup</strong> lets you change the playback speed of videos on Youtube, TikTok, Twitch,
 		Vimeo, Netflix, Prime Video, Disney+ and many more video and streaming platforms.
 	</p>
+	<div class="divider"></div>
 </div>
