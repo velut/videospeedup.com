61b2a93874994e9cb7ffa5c51e40f787f7776fc1
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-04 03:12:36 +0100

    chore: remove metadata file

--- src/lib/metadata.ts
+++
@@ -1,8 +0,0 @@
-export const projectName = 'videospeedup.com';
-export const projectNameStylized = 'VideoSpeedup.com';
-export const baseUrl = `https://www.videospeedup.com`;
-export const githubUrl = `https://github.com/velut/${projectName}`;
-export const githubIssuesUrl = `${githubUrl}/issues`;
-export const githubCommit = _GIT_COMMIT;
-export const githubCommitShort = githubCommit.slice(0, 7);
-export const githubCommitUrl = `${githubUrl}/commit/${githubCommit}`;
