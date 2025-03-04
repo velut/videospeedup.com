0fee39ca4e631b01f1037e3a1841a45d12be0775
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-04 03:12:24 +0100

    feat: update privacy policy page

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,66 +1,65 @@
 <script lang="ts">
-	import { githubIssuesUrl, projectNameStylized } from '$lib/metadata';
+	const ghIssueUrl = `https://github.com/velut/videospeedup.com/issues`;
 </script>
 
 <main class="flex items-center justify-center p-4 sm:p-8">
 	<article class="prose">
-		<div class="divider"></div>
-		<h1 class="my-0 text-center">Privacy Policy</h1>
-		<div class="divider"></div>
+		<hr />
+		<h1 class="text-center">Privacy policy</h1>
+		<hr />
 
 		<p>
-			This is the privacy policy for {projectNameStylized}.
+			This is the privacy policy for VideoSpeedup.com.
 			<br />
-			Last updated on July 22, 2023.
+			Last updated on March 4, 2025.
 		</p>
 
 		<ul>
 			<li>
 				<p>
-					<strong>Information We Collect</strong>
+					<strong>Information we collect</strong>
 				</p>
 				<p>On this website, we do not collect personal information from our visitors.</p>
 			</li>
 
 			<li>
 				<p>
-					<strong>First-party Cookies</strong>
+					<strong>First-party cookies</strong>
 				</p>
 				<p>
 					We save your application theme preference in <code>localStorage.theme</code> with a value
 					of <code>light</code> or <code>dark</code> when you explicitly click on the
-					<strong>Change Theme</strong> switch present in the footer.
+					<code>Change theme</code> switch present in the footer.
 				</p>
 			</li>
 
 			<li>
 				<p>
-					<strong>Server Provider</strong>
+					<strong>Server provider</strong>
 				</p>
 				<p>
-					This website is hosted by <a href="https://bunny.net/" target="_blank">Bunny.net</a>
-					, an EU based company, which may collect some data to provide its services. To learn more about
-					the data collected by Bunny.net, you can read their{' '}
-					<a href="https://bunny.net/privacy" target="_blank">privacy policy</a>
-					.
-				</p>
-			</li>
-
-			<li>
-				<p>
-					<strong>Server Logs</strong>
-				</p>
-				<p>
-					Our host, <a href="https://bunny.net/" target="_blank">Bunny.net</a>, provides us with
-					<a href="https://docs.bunny.net/docs/cdn-log-format" target="_blank">server logs</a> that
-					contain information about requests made to this website. Every IP address present in the
-					logs is automatically anonymized to <code>0.0.0.0</code> to prevent storing personal data.
+					This website is hosted by <a href="https://bunny.net/">Bunny.net</a>, an EU based company,
+					which may collect some data to provide its services. To learn more about the data
+					collected by Bunny.net, you can read their
+					<a href="https://bunny.net/privacy">privacy policy</a>.
 				</p>
 			</li>
 
 			<li>
 				<p>
-					<strong>External Websites</strong>
+					<strong>Server logs</strong>
+				</p>
+				<p>
+					Our host, <a href="https://bunny.net/">Bunny.net</a>, provides us with
+					<a href="https://docs.bunny.net/docs/cdn-log-format">server logs</a> that contain
+					information about requests made to this website. Every IP address present in the logs is
+					automatically anonymized to <code>0.0.0.0</code> to prevent storing personal data.
+				</p>
+			</li>
+
+			<li>
+				<p>
+					<strong>External websites</strong>
 				</p>
 				<p>
 					This website may contain links to external websites not operated by us and thus with
@@ -70,15 +69,13 @@
 			</li>
 
 			<li>
-				<p><strong>Contact Information</strong></p>
+				<p><strong>Contact information</strong></p>
 				<p>
-					You can contact the website owner, Edoardo Scibona, on{' '}
-					<a href={githubIssuesUrl} target="_blank">Github</a> or by{' '}
-					<a href="mailto:scibona.edoardo@proton.me">email</a>.
+					You can contact the website owner, Edoardo Scibona, on <a href={ghIssueUrl}>Github</a> or
+					by <a href="mailto:scibona.edoardo@proton.me">email</a>.
 				</p>
 			</li>
 		</ul>
-
-		<div class="divider"></div>
+		<hr />
 	</article>
 </main>
