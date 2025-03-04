4091adff55d0f2a06ba0c1b7c30e5d63e76d580f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-04 02:20:50 +0100

    feat: update Options

--- src/lib/components/Options.svelte
+++ src/lib/components/Options.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { optionAllMedia, optionFixedSpeed, optionIncludeAudio } from '$lib/stores';
+	import { options } from '$lib/state.svelte';
 	import IconHammerAndWrench from '~icons/noto/hammer-and-wrench';
 	import IconInfinity from '~icons/noto/infinity';
 	import IconMusicalNote from '~icons/noto/musical-note';
@@ -7,7 +7,7 @@
 </script>
 
 <h2 class="flex items-center gap-3">
-	<IconHammerAndWrench class="h-8 w-8" />
+	<IconHammerAndWrench class="size-8 flex-none" />
 	Options
 </h2>
 
@@ -18,61 +18,51 @@
 
 <div class="divider"></div>
 
-<div class="form-control">
-	<label class="label cursor-pointer gap-6">
-		<span class="label-text text-base"
-			><span class="flex items-center gap-2 font-bold"
-				><IconRepeatSingle class="h-5 w-5 flex-none" /> Speed controls just set their speed and never
-				1x.</span
-			><span class="mt-2 block text-sm"
-				>By default, speed controls alternate between their speed (for example, 3x) and the default
-				1x speed. Turn on this feature to have them just set their speed without going back to 1x.</span
-			>
-		</span>
-		<input
-			id="option-fixed-speed"
-			type="checkbox"
-			class="toggle"
-			bind:checked={$optionFixedSpeed}
-		/>
-	</label>
-</div>
+<label class="flex cursor-pointer items-center gap-6">
+	<span class="flex flex-col gap-2"
+		><span class="flex items-center gap-2 font-bold"
+			><IconRepeatSingle class="size-5 flex-none" /> Speed controls just set their speed and never 1x</span
+		><span class="text-sm"
+			>By default, speed controls alternate between their speed (for example, 3x) and the default 1x
+			speed. Turn on this feature to have them just set their speed without going back to 1x.</span
+		>
+	</span>
+	<input id="option-fixed-speed" type="checkbox" class="toggle" bind:checked={options.fixedSpeed} />
+</label>
 
 <div class="divider"></div>
 
-<div class="form-control">
-	<label class="label cursor-pointer gap-6">
-		<span class="label-text text-base"
-			><span class="flex items-center gap-2 font-bold"
-				><IconInfinity class="h-5 w-5 flex-none" /> Speed controls affect all media elements on a page.</span
-			><span class="mt-2 block text-sm"
-				>By default, speed controls only affect the first video in a page. Turn on this feature to
-				have them affect all videos in a page. This lets you watch multiple videos on TikTok,
-				Youtube Shorts or Twitter at faster speeds.</span
-			></span
-		>
-		<input id="option-all-media" type="checkbox" class="toggle" bind:checked={$optionAllMedia} />
-	</label>
-</div>
+<label class="flex cursor-pointer items-center gap-6">
+	<span class="flex flex-col gap-2"
+		><span class="flex items-center gap-2 font-bold"
+			><IconInfinity class="size-5 flex-none" /> Speed controls affect all media elements on a page</span
+		><span class="text-sm"
+			>By default, speed controls only affect the first video in a page. Turn on this feature to
+			have them affect all videos in a page. This lets you watch multiple videos on TikTok, Youtube
+			Shorts or Twitter at faster speeds.</span
+		></span
+	>
+	<input id="option-all-media" type="checkbox" class="toggle" bind:checked={options.allMedia} />
+</label>
 
 <div class="divider"></div>
 
-<div class="form-control">
-	<label class="label cursor-pointer gap-6">
-		<span class="label-text text-base"
-			><span class="flex items-center gap-2 font-bold"
-				><IconMusicalNote class="h-5 w-5 flex-none" /> Speed controls also affect audio-only elements.</span
-			><span class="mt-2 block text-sm"
-				>By default, speed controls only affect video elements in a page. Turn on this feature to
-				have them also affect audio-only elements in a page (The "All media elements" feature above
-				may also be needed for this to work).</span
-			></span
-		>
-		<input
-			id="option-include-audio"
-			type="checkbox"
-			class="toggle"
-			bind:checked={$optionIncludeAudio}
-		/>
-	</label>
-</div>
+<label class="flex cursor-pointer items-center gap-6">
+	<span class="flex flex-col gap-2"
+		><span class="flex items-center gap-2 font-bold"
+			><IconMusicalNote class="size-5 flex-none" /> Speed controls also affect audio-only elements</span
+		><span class="text-sm"
+			>By default, speed controls only affect video elements in a page. Turn on this feature to have
+			them also affect audio-only elements in a page (The "All media elements" feature above may
+			also be needed for this to work).</span
+		></span
+	>
+	<input
+		id="option-include-audio"
+		type="checkbox"
+		class="toggle"
+		bind:checked={options.includeAudio}
+	/>
+</label>
+
+<div class="divider"></div>
