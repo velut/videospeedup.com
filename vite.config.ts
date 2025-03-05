ac499f87d0a9b987e2eaea4bfd5394e2b103c86a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-05 20:02:16 +0100

    build: update licenses plugin to generate a txt summary

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { execSync } from 'node:child_process';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
-import { licenses } from './vite-plugin-licenses';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), Icons({ compiler: 'svelte' }), licenses()],
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		Icons({ compiler: 'svelte' }),
+		isProduction && (await import('./vite-plugin-licenses')).licenses()
+	],
 	define: {
 		__GIT_COMMIT: JSON.stringify(execSync('git rev-parse HEAD').toString().trim())
 	}
