900b6f33cc0432ac1d95d1bab9451aacd5f2a2aa
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-05 20:06:26 +0100

    refactor: remove licenses page

--- src/routes/licenses/+page.ts
+++
@@ -1,6 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = () => ({
-	title: 'Licenses for third-party software',
-	description: 'Licenses for third party software used on VideoSpeedup.com'
-});
