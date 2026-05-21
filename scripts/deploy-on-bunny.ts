aaeb84601aaf670150556e9ed50dfb2a5a25bf4c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2026-05-21 16:41:39 +0200

    ci: update deploy script for new bunny.net API

--- scripts/deploy-on-bunny.ts
+++ scripts/deploy-on-bunny.ts
@@ -33,7 +33,7 @@ async function deploy() {
 function getEnv(name: string): string {
   const value = process.env[name];
   if (!value) {
-    throw new Error(`${name} is: ${value}`);
+    throw new Error(`env ${name} is: ${value}`);
   }
   return value;
 }
@@ -45,34 +45,51 @@ async function clearStorageZone(endpoint: string, password: string) {
     throw new Error("clearStorageZone: invalid storage zone endpoint");
   }
 
-  // DELETE ALL FILES IN THE STORAGE ZONE.
+  // Bunny.net updated their API and we must delete files and directories one by one.
   const storageRootDir = `${endpoint}/`;
-  const deleteRes = await fetch(storageRootDir, {
-    method: "DELETE",
-    headers: { AccessKey: password },
-  });
-  if (deleteRes.status >= 400) {
-    console.error(storageRootDir);
-    console.error(deleteRes);
-    console.error(await deleteRes.json());
-    throw new Error("clearStorageZone: delete request failed to clear storage zone");
-  }
-
-  // Check if the storage zone was cleared.
-  await setTimeout(1000);
   const listRes = await fetch(storageRootDir, {
     method: "GET",
     headers: { accept: "application/json", AccessKey: password },
   });
   if (listRes.status >= 400) {
     console.error(await listRes.json());
-    throw new Error("clearStorageZone: cannot list storage zone files");
+    throw new Error("clearStorageZone: failed to list storage zone files");
   }
-  const data = await listRes.json();
+
+  // Delete top level entries.
+  const entries = (await listRes.json()) as { ObjectName: string; IsDirectory: boolean }[];
+  for (const entry of entries) {
+    const deleteRes = await fetch(
+      `${storageRootDir}${entry.ObjectName}${entry.IsDirectory ? "/" : ""}`,
+      {
+        method: "DELETE",
+        headers: { AccessKey: password },
+      },
+    );
+    if (deleteRes.status >= 400) {
+      console.error(await deleteRes.json());
+      throw new Error(
+        `clearStorageZone: failed to delete entry: ${entry.ObjectName} (IsDirectory: ${entry.IsDirectory})`,
+      );
+    }
+  }
+
+  // Check if the storage zone is clear.
+  await setTimeout(1000);
+  const emptyCheckRes = await fetch(storageRootDir, {
+    method: "GET",
+    headers: { accept: "application/json", AccessKey: password },
+  });
+  if (emptyCheckRes.status >= 400) {
+    console.error(await emptyCheckRes.json());
+    throw new Error("clearStorageZone: failed to check if the storage zone is empty");
+  }
+  const data = await emptyCheckRes.json();
   const isEmptyArray = Array.isArray(data) && data.length === 0;
   if (!isEmptyArray) {
     throw new Error("clearStorageZone: storage zone is not empty");
   }
+
   console.log("clearStorageZone: cleared storage zone");
 }
 
@@ -98,7 +115,13 @@ async function uploadFiles(buildDir: string, endpoint: string, password: string)
     console.log(`uploadFiles: uploaded: ${relativePath}`);
     uploadedFilesCount += 1;
   }
-  console.log(`uploadFiles: uploaded ${uploadedFilesCount} files`);
+  if (uploadedFilesCount < files.length) {
+    throw new Error(
+      `uploadFiles: failed to upload all files (total: ${files.length}, uploaded: ${uploadedFilesCount})`,
+    );
+  }
+
+  console.log(`uploadFiles: uploaded all ${uploadedFilesCount} files`);
 }
 
 async function purgePullZoneCache(pullZoneId: string, apiKey: string) {
@@ -110,5 +133,6 @@ async function purgePullZoneCache(pullZoneId: string, apiKey: string) {
     console.error(await res.json());
     throw new Error("purgePullZoneCache: failed to purge pull zone cache");
   }
+
   console.log("purgePullZoneCache: purged pull zone cache");
 }
